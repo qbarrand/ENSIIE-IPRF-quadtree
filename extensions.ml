941c74fc6eddf8c86c10565adf76bf7c104e74ac
Quentin Barrand <quentin@quba.fr>
2015-04-07 10:37:47 +0200

    Now using the coord type in the extension methods.

--- extensions.ml
+++ extensions.ml
@@ -1,10 +1,10 @@
 let c_equals =
-  fun (c1: float * float) ->
-  fun (c2: float * float) ->
+  fun (c1: coord) ->
+  fun (c2: coord) ->
   fst c1 = fst c2 && snd c1 = snd c2
 ;;
 
 let c_tostring =
-  fun (c: float*float) ->
+  fun (c: coord) ->
   Printf.printf "Point(%f, %f)\n" (fst c) (snd c)
 ;;
